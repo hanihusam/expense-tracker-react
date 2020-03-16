ac22160a316b59cca98379f4126c96ca0be711c4
Hani Husamuddin <hani.husam@gmail.com>
2020-03-16 21:52:26 +0700

    Fix idr format

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -6,10 +6,15 @@ const Balance = () => {
 
   const amounts = transactions.map(transaction => transaction.amount);
   const total = amounts.reduce((acc, item) => (acc += item), 0);
+
+  const sign = total < 0 ? "-" : "";
   return (
     <Fragment>
       <h4>Your Balance</h4>
-      <h1 id="balance">IDR {idrFormat(total)}</h1>
+      <h1 id="balance">
+        IDR {sign}
+        {idrFormat(Math.abs(total))}
+      </h1>
     </Fragment>
   );
 };
